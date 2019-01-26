535e692371cbf1773d8c9f09a7420e526bf2a59e
Ertuğrul BEKTİK <E.Bektik@gmail.com>
2019-01-26 13:34:30 +0300

    Animation

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,5 +1,6 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
   $scope.messages = [];
+  $scope.players = {};
   $scope.init = () => {
     const username = prompt('please enter username');
     if (username) {
@@ -22,10 +23,36 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
           const messageData = {
             type: 0, //info
             username: data.username,
+            text: 'katıldı.',
           };
           $scope.messages.push(messageData);
           $scope.$apply();
         });
+
+        socket.on('disUser', (data) => {
+          const messageData = {
+            type: 0, //info
+            username: data.username,
+            text: 'ayrıldı.',
+          };
+          $scope.messages.push(messageData);
+          $scope.$apply();
+        });
+
+        socket.on('initPlayers', (players) => {
+          $scope.players = players;
+          $scope.$apply();
+        });
+
+        let animate = false;
+        $scope.onClickPlayer = ($event) => {
+          if (!animate) {
+            animate = true;
+            $(`#${socket.id}`).animate({ left: $event.offsetX, top: $event.offsetY }, () => {
+              animate = false;
+            });
+          }
+        };
       }).catch((err) => {
         console.log(err);
       });
