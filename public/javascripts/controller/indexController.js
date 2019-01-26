2c249a10b14206997cc6ba16fc7e785f6b9c91c1
Ertuğrul BEKTİK <E.Bektik@gmail.com>
2019-01-26 23:26:13 +0300

    Bubble messages

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,4 +1,4 @@
-app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
+app.controller('indexController', ['$scope', 'indexFactory', 'configFactory', ($scope, indexFactory, configFactory) => {
   $scope.messages = [];
   $scope.players = {};
   $scope.init = () => {
@@ -16,85 +16,110 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
       element.scrollTop = element.scrollHeight;
     });
   }
+  function showBubble(id, message) {
+    $(`#${id}`).find('.message').html(message).show()
+      .delay(2000)
+      .fadeOut(500);
+  }
 
-  function initSocket(username) {
+  async function initSocket(username) {
     const connectionOptions = {
       reconnectionAttempts: 3,
       reconnectionDelay: 600
     };
 
-    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-      .then((socket) => {
-        socket.emit('newUser', { username });
-        socket.on('newUser', (data) => {
-          const messageData = {
-            type: 0, //info
-            username: data.username,
-            text: 'katıldı.',
-          };
-          $scope.messages.push(messageData);
-          $scope.players[data.id] = data;
-          $scope.$apply();
-        });
+    try {
+      const socketUrl = await configFactory.getConfig();
+      const socket = await indexFactory.connectSocket(socketUrl.data.socketUrl, connectionOptions);
 
-        socket.on('disUser', (data) => {
-          const messageData = {
-            type: 0, //info
-            username: data.username,
-            text: 'ayrıldı.',
-          };
-          $scope.messages.push(messageData);
-          delete $scope.players[data.id];
-          $scope.$apply();
-        });
+      socket.emit('newUser', {username});
+      socket.on('newUser', (data) => {
+        const messageData = {
+          type: 0, //info
+          username: data.username,
+          text: 'katıldı.',
+        };
+        $scope.messages.push(messageData);
+        $scope.players[data.id] = data;
+        $scope.$apply();
+        scrollTop();
+      });
 
-        socket.on('initPlayers', (players) => {
-          $scope.players = players;
-          $scope.$apply();
-        });
+      socket.on('disUser', (data) => {
+        const messageData = {
+          type: 0, //info
+          username: data.username,
+          text: 'ayrıldı.',
+        };
+        $scope.messages.push(messageData);
+        delete $scope.players[data.id];
+        $scope.$apply();
+        scrollTop();
+      });
 
-        socket.on('animate', (data) => {
-          $(`#${data.socketId}`).animate({ left: data.x, top: data.y }, () => {
+      socket.on('initPlayers', (players) => {
+        $scope.players = players;
+        $scope.$apply();
+      });
+
+      socket.on('animate', (data) => {
+        $(`#${data.socketId}`).animate({left: data.x, top: data.y}, () => {
+          animate = false;
+        });
+      });
+
+      let animate = false;
+      $scope.onClickPlayer = ($event) => {
+        if (!animate) {
+          const area = $('.gameArea');
+          const areaW = area.width();
+          const areaH = area.height() + 30;
+          console.log(areaH);
+          let x = $event.offsetX - 40;
+          let y = $event.offsetY - 40;
+          if (x < 40) {
+            x += 40;
+          } else if (x > areaW - 40) {
+            x = areaW - 50;
+          }
+          if (y < 40) {
+            y += 40;
+          } else if (y > areaH - 80) {
+            y -= 40;
+          }
+
+          socket.emit('animate', {x, y});
+
+          animate = true;
+          $(`#${socket.id}`).animate({left: x, top: y}, () => {
             animate = false;
           });
-        });
+        }
+      };
 
-        let animate = false;
-        $scope.onClickPlayer = ($event) => {
-          if (!animate) {
-            const x = $event.offsetX;
-            const y = $event.offsetY;
-
-            socket.emit('animate', { x, y });
-
-            animate = true;
-            $(`#${socket.id}`).animate({ left: x, top: y }, () => {
-              animate = false;
-            });
-          }
+      $scope.newMessage = () => {
+        const message = $scope.message;
+        const messageData = {
+          type: 1, //info
+          username,
+          text: message,
         };
+        socket.emit('newMessage', messageData);
 
-        $scope.newMessage = () => {
-          const message = $scope.message;
-          const messageData = {
-            type: 1, //info
-            username,
-            text: message,
-          };
-          socket.emit('newMessage', messageData);
+        $scope.messages.push(messageData);
+        $scope.message = '';
+        showBubble(socket.id, message);
+        scrollTop();
+      };
 
-          $scope.messages.push(messageData);
-          $scope.message = '';
-          scrollTop();
-        };
-
-        socket.on('newMessage', (message) => {
-          $scope.messages.push(message);
-          $scope.$apply();
-          scrollTop();
-        });
-      }).catch((err) => {
-        console.log(err);
+      socket.on('newMessage', (message) => {
+        $scope.messages.push(message);
+        $scope.$apply();
+        showBubble(message.socketId, message.text);
+        scrollTop();
       });
+    } catch (err) {
+      console.log(err);
+    }
   }
 }]);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const env = require('../config/env.json');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Live Balls' });
+});
+
+router.get('/getEnv', (req, res) => {
+  const envData = env[process.env.NODE_ENV || 'development'];
+  res.json(envData);
 });
 
 module.exports = router;
